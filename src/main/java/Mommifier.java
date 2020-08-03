cd3af0337db58e21cd87a77b1e3c86c1d779089b
unknown <dj9824@126.com>
2020-08-03 19:19:15 +0800

    [danjie] refactor: Mommifier class methods

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,19 +1,46 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class  Mommifier {
-
+    List<Character> vowelList = new ArrayList<>();
 
     public String mommify(String inputString) {
-        List<Character> vowelList = new ArrayList<>();
-        vowelList.add('a');
-        vowelList.add('e');
-        vowelList.add('i');
-        vowelList.add('o');
-        vowelList.add('u');
+        initList(vowelList);
         int vowelCount = 0;
+        vowelCount = setVowelCount(inputString, vowelCount);
+        List<Integer> continuousVowelIndexArray = new ArrayList<>();
+        if ((vowelCount / (float)inputString.length()) > 0.3) {
+            recordInsertPos(continuousVowelIndexArray, inputString, false);
+        }
+        StringBuffer inputStringBuffer = new StringBuffer(inputString);
+        insertMommy(inputStringBuffer, continuousVowelIndexArray);
+        return inputStringBuffer.toString();
+    }
+
+    private void insertMommy(StringBuffer inputStringBuffer, List<Integer> continuousVowelIndexArray) {
+        for (int insertArrayIndex = 0; insertArrayIndex < continuousVowelIndexArray.size(); ++insertArrayIndex) {
+            if (insertArrayIndex != 0) {
+                inputStringBuffer.insert(continuousVowelIndexArray.get(insertArrayIndex)
+                        + (5 * insertArrayIndex),"mommy");
+            } else {
+                inputStringBuffer.insert(continuousVowelIndexArray.get(insertArrayIndex),"mommy");
+            }
+        }
+    }
+
+    private void recordInsertPos(List<Integer> continuousVowelIndexArray, String inputString, boolean isLastCharVowel) {
+        for (int charIndex = 0; charIndex < inputString.length(); ++charIndex) {
+            if (vowelList.contains(inputString.charAt(charIndex)) && isLastCharVowel) {
+                continuousVowelIndexArray.add(charIndex);
+            } else if (vowelList.contains(inputString.charAt(charIndex)) && !isLastCharVowel) {
+                isLastCharVowel = true;
+            } else {
+                isLastCharVowel = false;
+            }
+        }
+    }
+
+    private int setVowelCount(String inputString, int vowelCount) {
         for (int index = 0; index < inputString.length(); ++index) {
             switch (inputString.charAt(index)) {
                 case 'a':
@@ -26,27 +53,14 @@ public class  Mommifier {
                     break;
             }
         }
-        boolean isLastCharVowel = false;
-        List<Integer> continuousVowelIndexArray = new ArrayList<>();
-        if ((vowelCount / (float)inputString.length()) > 0.3) {
-            for (int charIndex = 0; charIndex < inputString.length(); ++charIndex) {
-                if (vowelList.contains(inputString.charAt(charIndex)) && isLastCharVowel) {
-                    continuousVowelIndexArray.add(charIndex);
-                } else if (vowelList.contains(inputString.charAt(charIndex)) && !isLastCharVowel) {
-                    isLastCharVowel = true;
-                } else {
-                    isLastCharVowel = false;
-                }
-            }
-        }
-        StringBuffer inputStringBuffer = new StringBuffer(inputString);
-        for (int insertArrayIndex = 0; insertArrayIndex < continuousVowelIndexArray.size(); ++insertArrayIndex) {
-            if (insertArrayIndex != 0) {
-                inputStringBuffer.insert(continuousVowelIndexArray.get(insertArrayIndex) + (5 * insertArrayIndex),"mommy");
-            } else {
-                inputStringBuffer.insert(continuousVowelIndexArray.get(insertArrayIndex),"mommy");
-            }
-        }
-        return inputStringBuffer.toString();
+        return vowelCount;
+    }
+
+    private void initList(List<Character> vowelList) {
+        vowelList.add('a');
+        vowelList.add('e');
+        vowelList.add('i');
+        vowelList.add('o');
+        vowelList.add('u');
     }
 }
